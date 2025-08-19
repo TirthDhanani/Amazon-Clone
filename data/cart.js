3129bc9c764c1f882ffc3acf93b625cc6a77b403
tirthdhanani <tirthdhanani0243@gmail.com>
2025-08-19 17:59:00 +0530

    Save data and generate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -3,9 +3,11 @@ if (!cart){
     cart = [{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2,
+    deliveryOptionId: '1'
 },{
     productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
 }];
 }
 
@@ -24,7 +26,8 @@ export function addToCart(productId, quantity) {
         cart.push(
         {
             productId,
-            quantity 
+            quantity,
+            deliveryOptionId: '1' // Default delivery option
         }
     );
     }
