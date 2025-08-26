51c75f566c71505ab0c28a55bbf9ad77509a4aec
tirthdhanani <tirthdhanani0243@gmail.com>
2025-08-26 17:26:47 +0530

    Create automated tests for addToCart()

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) 
+export let cart;
+loadFromStorage();
+export function loadFromStorage(){
+    cart = JSON.parse(localStorage.getItem('cart')) 
 if (!cart){
     cart = [{
     productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -9,6 +12,8 @@ if (!cart){
     quantity: 1,
     deliveryOptionId: '2'
 }];
+
+}
 }
 
 function saveToStorage(){
@@ -21,7 +26,7 @@ export function addToCart(productId, quantity) {
         matchingItem = cartItem; 
     }})
     if (matchingItem) {
-        matchingItem.quantity  +=  quantity
+        matchingItem.quantity  +=  quantity || 1;
     } else {
         cart.push(
         {
