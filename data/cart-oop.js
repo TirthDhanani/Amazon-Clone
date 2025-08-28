1c25ca965a99e016681be844d737d25be65c314f
tirthdhanani <tirthdhanani0243@gmail.com>
2025-08-28 15:08:24 +0530

    Create oop version of cart

---
+++ data/cart-oop.js
@@ -0,0 +1,101 @@
+function Cart(localStorageKey) {
+    const cart = {
+    cartItems : undefined,
+    loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) 
+        if (!this.cartItems){
+            this.cartItems = [{
+            productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity : 2,
+            deliveryOptionId: '1'
+        },{
+            productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: '2'
+            }];
+        }
+    },
+    saveToStorage() {
+        localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems))
+    },
+    addToCart(productId, quantity) {
+        let matchingItem;
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId){
+            matchingItem = cartItem; 
+        }})
+        if (matchingItem) {
+            matchingItem.quantity  +=  quantity || 1;
+        } else {
+            this.cartItems.push(
+            {
+                productId,
+                quantity,
+                deliveryOptionId: '1' // Default delivery option
+            }
+        );
+        }
+        this.saveToStorage()
+    },
+    removeFromCart(productId) {
+        const newCart = [];
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId !== productId){
+                newCart.push(cartItem)
+            }
+
+        });
+        this.cartItems = newCart;
+        this.saveToStorage()
+    },
+    updateDeliveryOption(productId,deliveryOptionId) {
+        let matchingItem;
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId){
+                matchingItem = cartItem
+            }
+        })
+        matchingItem.deliveryOptionId = deliveryOptionId;
+        this.saveToStorage();
+    },
+    calculateCartQuantity(){
+    let cartQuantity = 0;
+        this.cartItems.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        })
+        this.saveToStorage()
+        return cartQuantity
+    },
+    saveQuantity(productId){
+        const quantity = document.querySelector(`.js-updated-quantity-${productId}`).value
+        if (quantity <= 0 || quantity > 100){
+            alert('Quantity entered is out of bounds')
+            return
+            
+        }else{
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId === productId){
+                    cartItem.quantity = parseInt(quantity)
+                }
+            })
+        }
+    } 
+};
+return cart;
+}
+
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
+
+
+
+cart.loadFromStorage();
+console.log(cart);
+businessCart.loadFromStorage();
+console.log(businessCart);
+
+
+
+
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart , loadFromStorage} from "../../data/cart.js";
+import { addToCart, cart , loadFromStorage, removeFromCart} from "../../data/cart.js";
 ;
 describe('test suite : addtoCart', () => {
     beforeEach(() => {
@@ -37,5 +37,21 @@ describe('test suite : addtoCart', () => {
         deliveryOptionId: '1'}]));
         expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart[0].quantity).toEqual(1);
+    })
+    it('remove a product from the cart', () => {
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([{
+                productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:1,
+                deliveryOptionId: '1'
+        }]);
+        });
+        loadFromStorage();
+        removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        removeFromCart('15b6fc6f-327a-4ec4-896f-486349e85a3d')
+
+        expect(cart.length).toEqual(0);
+        // expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        // expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
     })
 });
